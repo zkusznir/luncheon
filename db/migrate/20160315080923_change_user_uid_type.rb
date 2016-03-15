cf1817280bd5f3a255b83e6e4b412315294270b0
zkusznir <z.kusznir@gmail.com>
2016-03-15 09:19:40 +0100

    Change User's uid column type to string

---
+++ db/migrate/20160315080923_change_user_uid_type.rb
@@ -0,0 +1,5 @@
+class ChangeUserUidType < ActiveRecord::Migration
+  def change
+    change_column :users, :uid, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314190827) do
+ActiveRecord::Schema.define(version: 20160315080923) do
 
   create_table "meals", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160314190827) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer  "uid"
+    t.string   "uid"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
